487bdad37f7ceb9f01dbdf7ab819bf3ef5cff975
SeKreT1179 <smert1179@gmail.com>
2025-06-03 19:15:09 +0300

    HomeWork300525

---
+++ HomeWork300525/src/App334.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App334 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        for (int i = a; a <= i && i <= b; i++) {
+            if (i % d == c) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
