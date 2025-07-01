ac6a3b0bd433ca731fec60fbfcdd34fd1a575913
SeKreT1179 <smert1179@gmail.com>
2025-07-01 20:56:30 +0300

    endKR

---
+++ KR/src/App41.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mas[][] = new int[sc.nextInt()][sc.nextInt()];
+    }
+
+    public static void comp(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mas[][] = new int[sc.nextInt()][sc.nextInt()];
+        {
+
+        }
+    }
+}
