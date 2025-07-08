6119fd7ab3112c5aeb3845fabb622298580215e6
SeKreT1179 <smert1179@gmail.com>
2025-07-08 19:06:31 +0300

    отчёт

--- KR/src/App41.java
+++ KR/src/App41.java
@@ -5,15 +5,54 @@ public class App41 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int mas[][] = new int[sc.nextInt()][sc.nextInt()];
+        int[][] ar1 = new int[5][4];
+        int[][] ar2 = fillArrayRandomInt(ar1);
+
+        printArray(ar1);
+        int[] sumColumnArray = sumColumnArray(ar1);
+        printArray(sumColumnArray);
     }
 
-    public static void comp(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int mas[][] = new int[sc.nextInt()][sc.nextInt()];
-        {
-
+//__________________________________________________________________________________________________//
+//с помощью этой части коды мы заполняем масив
+    public static int[][] fillArrayRandomInt(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 150);
+            }
         }
+        return array;
+    }
+
+//__________________________________________________________________________________________________//
+
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+//__________________________________________________________________________________________________//
+
+    public static int[] sumColumnArray(int[][] array) {
+        int[] res = new int[array[0].length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                res[j] += array[i][j];
+            }
+        }
+        return res;
+    }
+
+//__________________________________________________________________________________________________//
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println();
     }
 }
